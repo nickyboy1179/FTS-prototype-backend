440e1ac6b7b8cb949d731d212e497c14be658793
Nicky Houter <101206236+nickyboy1179@users.noreply.github.com>
2023-12-09 00:23:54 +0100

    Added .env to .gitignore

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask
+from openai import OpenAI
 
 app = Flask(__name__)
+
+client =
 
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
+
+
+@app.route('/test')
+def test_api():
+    return None;
 
 
 if __name__ == '__main__':
