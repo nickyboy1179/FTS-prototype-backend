16b0e1682ffae88ad42a1624d4adaf21af1d5d29
Nicky Houter <101206236+nickyboy1179@users.noreply.github.com>
2023-12-16 01:33:10 +0100

    Started experimenting with audio files for translations

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os
+import os, json
 from flask import Flask
 from openai import OpenAI
 from dotenv import load_dotenv
@@ -11,25 +11,36 @@ client = OpenAI(
     api_key=os.environ.get("OPENAI_API_KEY"),
 )
 
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "Say this is a test",
-        }
-    ],
-    model="gpt-3.5-turbo",
+# chat_completion = client.chat.completions.create(
+#     messages=[
+#         {
+#             "role": "user",
+#             "content": "What is your name?",
+#         }
+#     ],
+#     model="gpt-3.5-turbo",
+# )
+#
+# print(chat_completion.choices[0].message.content)
+audio_file = open("Testaudio.mp3", "rb")
+transcript = client.audio.transcriptions.create(
+    model="whisper-1",
+    file=audio_file
 )
+
+audio_file = open("Testaudio.mp3", "rb")
+translation = client.audio.translations.create(
+    model="whisper-1",
+    file=audio_file
+)
+
+print(transcript)
+print(translation)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
-
-
-@app.route('/test')
-def test_api():
-    return chat_completion
 
 
 if __name__ == '__main__':
