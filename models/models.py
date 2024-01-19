b4f5132fc3b16c2898c180e867ad2c32d932846a
Nicky Houter <101206236+nickyboy1179@users.noreply.github.com>
2024-01-19 02:25:36 +0100

    Database contains all data now

--- models/models.py
+++ models/models.py
@@ -12,30 +12,47 @@ class Event(db.Model):
     end_time = db.Column(db.Time, nullable=False)
     location_id = db.Column(db.Integer, db.ForeignKey('location.id'), nullable=False)
 
+    def __repr__(self):
+        return f'<Event: {self.id} {self.name}'
+
 
 class Location(db.Model):
     __tablename__ = 'location'
     id = db.Column(db.Integer, primary_key=True)
     location_name = db.Column(db.String(50))
     street_name = db.Column(db.String(255), nullable=False)
-    postal_code = db.Column(db.String(255), nullable=False)
     house_number = db.Column(db.Integer, nullable=False)
+    location_notes = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'Location: {self.id} {self.street_name} {self.house_number}'
 
 
 class EventDays(db.Model):
     __tablename__ = 'event_days'
     id = db.Column(db.Integer, primary_key=True)
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'), nullable=False)
     day_of_week = db.Column(db.String(9), nullable=False)
     week_of_month = db.Column(db.Integer)
 
+    def __repr__(self):
+        return f'Location: {self.id} {self.event_id} {self.day_of_week} {self.week_of_month}'
+
 
 class EventCategory(db.Model):
-    __tablename = 'event_category'
-    category_id = db.Column(db.Integerm, db.ForeignKey('category.id'), primary_key=True)
-    event_id = db.Column(db.Intergerm, db.ForeignKey('event.id'), primary_key=True)
+    __tablename__ = 'event_category'
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), primary_key=True)
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'), primary_key=True)
+
+    def __repr__(self):
+        return f'Location: {self.category_id} {self.event_id}'
 
 
 class Category(db.Model):
     __tablename__ = 'category'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(50), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f'Location: {self.id} {self.name}'
+
