09104c47b8ae81e5727ce91d3c2bf28e1d15cf08
Nicky Houter <101206236+nickyboy1179@users.noreply.github.com>
2024-01-22 03:51:48 +0100

    Added start script

---
+++ startscript.bat
@@ -0,0 +1,13 @@
+call pip install flask
+call pip install openai
+call pip install python-dotenv
+call pip install flask-socketio
+call pip install flask-sqlalchemy
+call pip install flask-migrate
+
+call python app.py
+
+
+
+
+
