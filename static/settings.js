9d759728574f05dc27ba74d5c4728005a956056d
Nicky Houter <101206236+nickyboy1179@users.noreply.github.com>
2024-02-09 14:31:52 +0100

    added messages for submission on settings page

--- static/settings.js
+++ static/settings.js
@@ -1,6 +1,15 @@
 const send_to_assist = document.querySelector('#send_to_assistant')
+const form = document.querySelector('form')
+const form_message = document.querySelector("#update_form")
+const assistant_message = document.querySelector('#update_assistant')
 
 send_to_assist.addEventListener('click', sendToAssistant)
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    displayUpdateMessage(form_message)
+    setTimeout(() => form.submit(), 1000)
+} )
+
 
 function sendToAssistant() {
     fetch('/send-to-assistant', {
@@ -9,5 +18,11 @@ function sendToAssistant() {
         .then(response => response.json())
         .then(data => {
             console.log(data)
+            displayUpdateMessage(assistant_message)
         })
+}
+
+
+function displayUpdateMessage(div) {
+    div.style.display = 'block';
 }
